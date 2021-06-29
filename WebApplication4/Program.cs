fb896b3823b06f8b3122824d54bfb9100c5f8606
Robert Untalan <robertuntal@gmail.com>
2021-06-29 04:11:26 -0700

    Testing Commit

--- WebApplication4/Program.cs
+++ WebApplication4/Program.cs
@@ -14,6 +14,7 @@ namespace WebApplication4
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            Console.WriteLine("Testing"); 
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
